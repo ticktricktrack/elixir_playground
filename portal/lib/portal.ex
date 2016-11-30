24373d0cc09a13bd836974927711c35215fda401
Rainer Kuhn <ticktricktrack@gmail.com>
2016-11-30 11:22:21 +0000

    push right through portal

--- portal/lib/portal.ex
+++ portal/lib/portal.ex
@@ -17,4 +17,22 @@ defmodule Portal do
     opts = [strategy: :one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defstruct [:left, :right]
+
+  def transfer(left, right, data) do
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    %Portal{left: left, right: right}
+  end
+
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
+      :error   -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+    portal
+  end
 end
